ac926f56d52ade5c0d79bdc45dfed4ac9edf5ae5
matsuzaki <shin.matsu.p@gmail.com>
2019-09-19 17:35:12 +0900

    インスタンスメソッド info() を追加した

--- oop.py
+++ oop.py
@@ -3,17 +3,19 @@ class Person:
         self.name = name
         self.age = age
 
+    def info(self):
+        return f'{self.name}は{self.age}歳です'
+
 
 def main():
     # インスタンス化
     bob = Person(name='Bob', age=18)
-    print(bob)
-    print(bob.name)
-    print(bob.age)
-
-    tom = Person(name='Tom', age=40)
-    print(tom)
-    print(tom.name)
+    tom = Person(name='Tom', age=30)
+    # print(bob)
+    # print(bob.name)
+    # print(bob.age)
+    print(bob.info())
+    print(tom.info())
 
 
 if __name__ == '__main__':
