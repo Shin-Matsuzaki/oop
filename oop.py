d2ef23f9c4b869ddd951d6e6423e8aa9231ab3ab
matsuzaki <shin.matsu.p@gmail.com>
2019-09-19 17:21:07 +0900

    クラスの定義とインスタンスの生成を練習した

--- oop.py
+++ oop.py
@@ -1,13 +1,19 @@
 class Person:
-    def __init__(self, name):
+    def __init__(self, name, age):
         self.name = name
+        self.age = age
 
 
 def main():
     # インスタンス化
-    bob = Person(name='Bob')
+    bob = Person(name='Bob', age=18)
     print(bob)
     print(bob.name)
+    print(bob.age)
+
+    tom = Person(name='Tom', age=40)
+    print(tom)
+    print(tom.name)
 
 
 if __name__ == '__main__':
